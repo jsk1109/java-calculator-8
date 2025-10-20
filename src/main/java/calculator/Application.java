9a089bff4ca57d1546b87b050d5b6dcc2dc9f115
jsk11 <rmwl1235@gmail.com>
2025-10-20 18:53:08 +0900

    예외처리 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-
-import java.sql.SQLOutput;
-import java.util.ArrayList;
+import java.util.regex.Pattern;
 import java.util.Arrays;
 
 public class Application {
@@ -11,39 +9,45 @@ public class Application {
         System.out.print("덧셈할 문자열을 입력해 주세요.\n");
         String input = Console.readLine();
         String[] list;
-
-       if (input == null || input.equals("")){
-           System.out.println("결과 : 0");
-           return;
-       }
-
-        if(input.startsWith("//")){
-            String[] parts = input.split("\n",2);
-            String starts = parts[0];
-            String custom = starts.substring(2);
+        if (input == null || input.equals("")) {
+            System.out.println("결과 : 0");
+            return;
+        }
+        if (input.startsWith("//")) {
+            String pInput = input.replace("\\n","\n");
+            String[] parts = pInput.split("\n", 2);
+            if (parts.length < 2) {
+                throw new IllegalArgumentException("커스텀 구분자 형식 오류 (\\n이 누락되었습니다.)");
+            }
+            String header = parts[0];
+            String customDelimiter = header.substring(2);
+            if (customDelimiter.equals("")) {
+                throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.");
+            }
             String numberString = parts[1];
-            list = numberString.split(custom);
-        }else{
+            list = numberString.split(Pattern.quote(customDelimiter));
+        } else {
             list = input.split(",|:");
         }
-
         int sum = 0;
-        for(int i = 0; i < list.length; i++) {
-            if(list[i].equals("")) {
+        for (int i = 0; i < list.length; i++) {
+
+            // 4-1. 빈 문자열("")은 0으로 처리
+            if (list[i].equals("")) {
                 sum = sum + 0;
-            }else{
+            } else {
                 try {
                     int number = Integer.parseInt(list[i]);
+
                     if (number < 0) {
-                        throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
+                        throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
                     }
                     sum = sum + number;
-                }catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
                 }
             }
         }
         System.out.println("결과 : " + sum);
-
     }
 }
